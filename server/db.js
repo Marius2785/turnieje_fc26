ce5c9cfe08a623446a66d82616a4c4c2134a9f71
Marius2785 <fyggehf957@gmail.com>
2026-01-14 22:18:16 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,1 +1,42 @@
+import sqlite3 from "sqlite3";
 
+export const db = new sqlite3.Database("database.sqlite");
+
+db.serialize(() => {
+  db.run(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      login TEXT UNIQUE,
+      password TEXT,
+      balance INTEGER DEFAULT 1000,
+      role TEXT DEFAULT 'user'
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS matches (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      a TEXT,
+      b TEXT,
+      oddsA REAL,
+      oddsD REAL,
+      oddsB REAL
+    )
+  `);
+
+  db.run(`
+    CREATE TABLE IF NOT EXISTS bets (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      user_id INTEGER,
+      match_id INTEGER,
+      pick TEXT,
+      amount INTEGER
+    )
+  `);
+
+  // ADMIN NA START
+  db.run(
+    `INSERT OR IGNORE INTO users (login,password,role,balance)
+     VALUES ('administrator','małpyigoryle23_','admin',100000)`
+  );
+});
