7cd299e0f557b3a622c4f308c8042753918941d9
Marius2785 <fyggehf957@gmail.com>
2026-01-14 02:20:39 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,16 +3,15 @@ export const db = new sqlite3.Database("database.sqlite");
 
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
-    discord_id TEXT UNIQUE,
-    username TEXT,
-    avatar TEXT,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    login TEXT UNIQUE,
+    password TEXT,
     balance INTEGER DEFAULT 1000,
-    role TEXT
+    role TEXT DEFAULT 'user'
   )`);
 
   db.run(`CREATE TABLE IF NOT EXISTS matches (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     playerA TEXT,
     playerB TEXT,
     status TEXT,
@@ -20,5 +19,13 @@ db.serialize(() => {
     oddsD REAL,
     oddsB REAL,
     result TEXT
+  )`);
+
+  db.run(`CREATE TABLE IF NOT EXISTS bets (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER,
+    match_id INTEGER,
+    choice TEXT,
+    amount INTEGER
   )`);
 });
